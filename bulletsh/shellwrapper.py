32cb57930933c5fb82d98c988e7a21175f5e28d5
Adam Dad <adam-dad@hotmail.co.uk>
2021-08-28 01:09:57 +0100

    completion time and cd ls

--- bulletsh/shellwrapper.py
+++ bulletsh/shellwrapper.py
@@ -1,4 +1,7 @@
+import os
 import subprocess
+import sys
+import time
 from cmd import Cmd
 
 from pushbullet import Pushbullet
@@ -6,13 +9,31 @@ from pushbullet import Pushbullet
 
 class ShellWrapper(Cmd):
     intro = ""
-    prompt = "bsh ###>"
+    prompt = "bsh | " + os.getcwd() + " ###>"
 
     def __init__(self, token) -> None:
         super().__init__()
         self.token = token
 
-    def default(self, line):
-        proc = subprocess.run(line.split(' '), stderr=subprocess.PIPE, text=True)
+    def push_result(self, line, process, completion_time) -> None:
+        note_body = "rcode: " + str(process.returncode) + "\nSUCCESS!" + "\nCompleted in: " + str(round(completion_time/60, 3)) + "m"
         pb = Pushbullet(self.token)
-        pb.push_note(line, str(proc.stderr) + "\nFAILED :(") if proc.returncode != 0 else pb.push_note(line, "rcode: " + str(proc.returncode) + "\nSUCCESS!")
+        pb.push_note(line, str(process.stderr) + "\nFAILED :(") if process.returncode != 0 else pb.push_note(line, note_body)
+
+    def default(self, line) -> None:
+        start_time = time.perf_counter()
+        proc = subprocess.run(line.split(' '), stderr=subprocess.PIPE, text=True)
+        end_time = time.perf_counter()
+
+        self.push_result(line, proc, end_time-start_time)
+
+    def do_cd(self, arg) -> None:
+        os.chdir(arg)
+        self.prompt = "bsh | " + os.getcwd() + " ###>"
+
+    def do_ls(self, arg) -> None:
+        proc = subprocess.run(["ls", "-lah"])
+
+    def do_q(self, arg) -> None:
+        print("bye :'(")
+        sys.exit()
